d316952dce6477457270e0218a8c76314a1496bd
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-10-28 01:33:56 +0300

    added to my files

---
+++ pokemon.js
@@ -0,0 +1,280 @@
+let cards = [{ // add your pokemon here 
+    "name": "Picatchu",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+    "type": "Electric ",
+    "abilities": "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
+    "Category": "Mouse"
+}, {
+    "name": "Charizard",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+    "desc": "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+    "type": "flying ",
+    "abilities": "Blaze ",
+    "Category": "Flame" 
+}, {
+    "name": "Wartortle",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+    "desc":"It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.",
+    "type": "Water",
+    "abilities": "Torrent",
+    "Category": "Turtle"
+
+},{
+    "name": "Butterfree",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
+    "desc" : "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+    "type": "Bug , Flying",
+    "abilities": "Compound Eyes",
+    "Category": "Butterfly"
+}, {
+    "name": "Nidorina",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png",
+    "desc": "The horn on its head has atrophied. It’s thought that this happens so Nidorina’s children won’t get poked while their mother is feeding them.",
+    "type": "Poison ",
+    "abilities": "Poison Poin , Rivalry",
+    "Category": "Poison Pin " 
+}, {
+    "name": "Charmeleon",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+    "type": "Fire",
+    "abilities": "It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.",
+     "Category": "Flame"
+}, {
+    "name": "Squirtle",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    "type": "water ",
+    "abilities": "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    "Category": "Tiny Turtl"
+
+},{
+    "name": "Clefable",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/036.png",
+    "type": "fire",
+    "abilities": "A timid fairy Pokémon that is rarely seen, it will run and hide the moment it senses people.",
+    "Category": "Fairy"
+}, {
+    "name": "Kirlia",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/281.png",
+    "type": "Emotion  ",
+    "abilities": "If its Trainer becomes happy, it overflows with energy, dancing joyously while spinning about. ",
+    "Category": "Psychic ,Fairy" 
+}, {
+    "name": "Vulpix",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png",
+    "type": "Fire ",
+    "abilities": "While young, it has six gorgeous tails. When it grows, several new tails are sprouted",
+    "Category": "fox"
+ },{
+        "name": "Ponyta",
+        "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png",
+        "type": "Fire ",
+        "abilities": "It can’t run properly when it’s newly born. As it races around with others of its kind, its legs grow stronger.",
+        "Category": "Fire Horse"
+    }, {
+        "name": "Caterpie",
+        "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+        "type": "Bug ",
+        "abilities": "For protection, it releases a horrible stench from the antenna on its head to drive away enemies.",
+        "Category": "Worm" 
+    }, 
+    {
+        "name": "Oddish",
+        "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/043.png",
+        "type": "Grass , Poison ",
+        "abilities": "If exposed to moonlight, it starts to move. It roams far and wide at night to scatter its seeds.",
+        "Category": "Weed"
+    },{
+    "name": "Ninetales",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png",
+    "type": "Fire ",
+    "abilities": "It is said to live 1,000 years, and each of its tails is loaded with supernatural powers.",
+    "Category": "Fox"
+}, {
+    "name": "Parasect",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/047.png",
+    "type": "Bug ,Grass",
+    "abilities": "The bug host is drained of energy by the mushroom on its back. The mushroom appears to do all the thinking.",
+    "Category": "Mushroom" 
+}, {
+    "name": "Meowth",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png",
+    "type": "Normal",
+    "abilities": "It loves to collect shiny things. If it’s in a good mood, it might even let its Trainer have a look at its hoard of treasures. ",
+    "Category": "Scratch Cat"
+
+}, {
+    "name": "Wigglytuff",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png",
+    "type": "Normal ,Fairy",
+    "abilities": "The more air it takes in, the more it inflates. If opponents catch it in a bad mood, it will inflate itself to an enormous size to intimidate them.",
+    "Category": "Baloon"
+
+}, {
+    "name": "Machoke",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/067.png",
+    "type": "Fighting ",
+    "abilities": "Its muscular body is so powerful, it must wear a power-save belt to be able to regulate its motions.",
+    "Category": "Superpower"
+
+}, {
+    "name": "Golduck",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/055.png",
+    "type": "Water ",
+    "abilities": "This Pokémon lives in gently flowing rivers. It paddles through the water with its long limbs, putting its graceful swimming skills on display.",
+    "Category": "Duch"
+
+}, {
+    "name": "Geodude",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png",
+    "type": "Rick, Ground",
+    "abilities": "Commonly found near mountain trails and the like. If you step on one by accident, it gets angry.",
+    "Category": "Rock"
+
+}, {
+    "name": "Slowpoke",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/079.png",
+    "type": "Water ",
+    "abilities": "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either..",
+    "Category": "Doupy"
+
+}, {
+    "name": "Farfetch’d",
+    "imgPath": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/083.png",
+    "type": "Rick, Ground",
+    "abilities": "",
+    "Category": "Rock"
+
+}, 
+];
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(event) { // onload create all pokemin images
+//     if (window.location.hash) {;
+//     } else;
+// });
+
+
+function renderCards(self) {
+    document.getElementById('showPokemons').innerHTML = "";
+    let inputValue = document.getElementById('inputValue').value || 0; //get input value on search
+
+
+    for (let i = 0; i < cards.length; i++) { //  ((loop cards  ))
+        if (self == "btnclicked" && Compare(cards[i].name.toLowerCase(), inputValue.toLowerCase()) == true) {
+             //////create all pokemon on page searched
+            document.getElementById('showPokemons').innerHTML =`
+             <div class="col-md-2">
+            <div class="thumbnail" >
+            <a href="pokemondetail.html#` + cards[i].name + `" target="_blank">
+            <img src=` + cards[i].imgPath + ` alt=` + cards[i].name + ` style="width:100% "> </a>
+            <div class="caption text-center">
+            <svg id=` + i + `  onclick="storetoLocalStorage(this)" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+            </svg>
+            <h3>` + cards[i].name + `</h3>
+             </div>  
+            </div>
+        </div>`
+        } else if (self == "load")
+         {
+            //create all pokemon 
+            document.getElementById('showPokemons').innerHTML += ` 
+            <div class="col-md-2">
+              <div class="thumbnail" >
+             <a href="pokemondetail.html#` + cards[i].name + `" target="_blank">
+            <img src=` + cards[i].imgPath + ` alt=` + cards[i].name + `"> </a>
+            <div class="caption text-center">
+            <svg  id=` + i + `  onclick="storetoLocalStorage(this)" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+                <h3>` + cards[i].name + `</h3>
+              
+            </div>
+    </div>
+</div>`
+        }
+    }
+}
+
+
+function Compare(s1, s2) { 
+    if (s1 == s2) return true;
+    else
+     return false;
+}
+let pokemonnotes = [];
+let keysPokemon = "pokemonLocalSearchValues";
+
+ ///////////// store  in localStorage
+ 
+   function storetoLocalStorage(self) {
+    let id = self.id;
+    document.getElementById(id).style.color = "RED";
+    let getjsonpokemonnotes = localStorage.getItem(keysPokemon);
+    if (getjsonpokemonnotes == null) {
+
+        let note = {};
+        note.id = id;
+   
+        pokemonnotes.push(note);
+        localStorage.setItem(keysPokemon, JSON.stringify(pokemonnotes));
+    } 
+    else {
+        pokemonnotes = JSON.parse(getjsonpokemonnotes);
+        for (i = 0; i < pokemonnotes.length; i++) {
+            pokemonnotes[i].id = pokemonnotes[i].id;
+        }
+        let note = {};
+        note.id = id;
+
+        pokemonnotes.push(note);
+        let updatedjsonpokemonnotes = JSON.stringify(pokemonnotes);
+        localStorage.setItem(keysPokemon, updatedjsonpokemonnotes);
+    }
+}
+/////////// next page desscription 
+   function pokemonDesc() {  
+   if (window.location.hash) {
+       for (let i = 0; i < cards.length; i++) {
+           if (cards[i].name == window.location.hash.substr(1, )) {
+               document.getElementById('showPokemonsDetails').innerHTML = ` <div class="col-md-12">
+           <div class="d-flex p-12"  >
+           <img class="img-responsive center-block" src=` + cards[i].imgPath + ` alt=` + cards[i].name + ` width="340" height="370"  >
+           <div class="caption text-center">
+               <h3>` + cards[i].name + `</h3> </div>
+       <p  class="caption text-center"> Type: ` + cards[i].type + `</p>
+       <p  class="caption text-center">Abilities: ` + cards[i].abilities + `</p>
+       <p  class="caption text-center"> Category: ` + cards[i].Category + `</p>
+   </div>
+</div>`
+           }
+       }
+   
+   }
+}
+
+function favPokemon() {////favvv function
+ var getjsonpokemonnotes = localStorage.getItem(keysPokemon);
+ if (getjsonpokemonnotes != null) {
+    pokemonnotes = JSON.parse(getjsonpokemonnotes);
+     for (i = 0; i < pokemonnotes.length; i++) {
+      document.getElementById('showfevPokemons').innerHTML += ` <div class="col-md-2">
+     <div class="thumbnail" >
+      <a href="pokemondetail.html#` + cards[pokemonnotes[i].id].name + `" target="_blank">
+     <img src=` + cards[pokemonnotes[i].id].imgPath + ` alt=` + cards[pokemonnotes[i].id].name + ` style="width:100%"> </a>
+      <div class="caption text-center">
+     <svg color="RED" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+       path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+      <h3>` + cards[pokemonnotes[i].id].name + `</h3>
+        </div>
+         </div>
+        </div>`
+        
+        }
+    }
+}
+
+
